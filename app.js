29d114c5efea726181f66a8511326c8c69e7d312
Tadhg <tadhgsmboyle@gmail.com>
2021-03-09 15:26:16 -0800

    :tada: Initial Commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const goodNameOne = 1;
+const goodNameTwo = false;
+
+let goodNameThree = "tadhg";
+let goodNameFour = 5;
+
+if (goodNameOne === goodNameFour) {
+    console.log('they match');
+} else {
+    console.log('no luck');
+}
+
+for (s of goodNameThree) {
+    console.log(s);
+}
